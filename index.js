94bcbe703abd2ada6e5cf940fd45fd3f579d9624
nminh0402 <minhpro0402@gmail.com>
2026-04-22 21:39:09 +0700

    Manual Counter created

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let username;
 
-document.getElementById("mySubmit").onclick = function() {
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtnBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
 }
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
+
